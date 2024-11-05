6e087ff030e43f97657c20829b2cdc33697093b0
IamRay13 <ramontariao@gmail.com>
2024-11-05 15:44:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ document.getElementById('schedulingDocument').addEventListener('change', functio
             textArea.value = "What's needed: Scheduling/ 60 Month Restart\n| RUL: \n| SWO Expiration: \n| Signed date: \n| Item/s: E1390 & \n| Comment/s:\nWe got valid testing and received signed SWO by CODE. \nInstructed not to log Medicare.\nCreated New Sales Order - CODE.\nNew Pickup/Exchange - CODE.\nSB flipped to Restart Scheduled.";
             break;
         case 'nonMedicare':
-            textArea.value = "What's needed: Scheduling/ 60 Month Restart\n| RUL: \n| SWO Expiration: \n| Signed date: \n| Item/s: E1390 & \n| Comment/s:\nWe got valid testing and received signed SWO by CODE. \nInstructed not to log Non-Medicare.\nCreated New Sales Order - CODE.\nNew Pickup/Exchange - CODE.\nSB flipped to Restart Scheduled.";
+            textArea.value = "What's needed: Scheduling/ 60 Month Restart\n| RUL: \n| SWO Expiration: \n| Signed date: \n| Item/s: E1390 & \n| Comment/s:\nWe got valid testing and received signed SWO by CODE. \nLogged CMN.\nCreated New Sales Order - CODE.\nNew Pickup/Exchange - CODE.\nSB flipped to Restart Scheduled.";
             break;
         default:
             textArea.value = "";
