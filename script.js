51974c50ddbec67f716bc934b7e2a58b952a3794
IamRay13 <ramontariao@gmail.com>
2024-11-06 09:14:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,10 +24,10 @@ document.getElementById('faxDocument').addEventListener('change', function() {
             textArea.value = "IMPORTANT!\n\nThe answer keys are located on the back of the sheet. We kindly request that the doctor accurately and completely fill out the form for insurance billing purposes on behalf of the patient, please ensure that the dates and signature should be in the same format.\n\nPlease have DOCTOR sign and fax it back to (484) 362-1480 at your earliest convenience.\n\nFeel free to call us at (484) 567-0666 if you have any questions.\nThank you for your prompt attention!\n\n----------------------------------------------------------\nFrom: Tariao, Raymond\nPhone: (484) 567-0666\nFax: (484) 362-1480\n\nPhone: 4845670666\nFax: 4843621480";
             break;
         case '6mwt':
-            textArea.value = "Good day! Patient is due for 5-year O2 requalification. Requesting a copy of a qualifying 6-minute walk test from any year to be used in requalifying patient. For questions, you may call 484-567-0666. Thank you!";
+            textArea.value = "Good day! Patient is due for 5-year O2 requalification. Requesting a copy of a qualifying 6-minute walk test from any year to be used in requalifying patient. For questions, you may call 484-567-0666. Thank you!\n\n----------------------------------------------------------\nFrom: Tariao, Raymond\nPhone: (484) 567-0666\nFax: (484) 362-1480\n\nPhone: 4845670666\nFax: 4843621480";
             break;
         case 'tss':
-            textArea.value = "Good day! Patient is due for 5-year O2 requalification. Requesting a copy of a qualifying Titration Sleep Study from any year to be used in requalifying patient. For questions, you may call 484-567-0666. Thank you!";
+            textArea.value = "Good day! Patient is due for 5-year O2 requalification. Requesting a copy of a qualifying Titration Sleep Study from any year to be used in requalifying patient. For questions, you may call 484-567-0666. Thank you!\n\n----------------------------------------------------------\nFrom: Tariao, Raymond\nPhone: (484) 567-0666\nFax: (484) 362-1480\n\nPhone: 4845670666\nFax: 4843621480";
             break;
         case 'pocRx':
             textArea.value = "*****IMPORTANT*****\n\nWe would like to request for a prescription for a Portable Oxygen Concentrator for the patient, [Patient's Full Name], as it is more convenient and lighter for her needs.\n\nPlease send the prescription to Main Fax: (484) 362-1480\nAlternate Fax#: (866) 503-8207 at your earliest convenience.";
